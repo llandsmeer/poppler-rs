827b77ef2cea8d4b8f60d64e608d3c292cd2336f
Lennart Landsmeer <lennart@landsmeer.email>
2018-09-19 07:59:38 +0200

    cairo::Context::to_raw_none (&mut -> &)

--- src/lib.rs
+++ src/lib.rs
@@ -97,11 +97,11 @@ impl PopplerPage {
         (width, height)
     }
 
-    pub fn render(&self, ctx: &mut cairo::Context) {
+    pub fn render(&self, ctx: &cairo::Context) {
         unsafe { ffi::poppler_page_render(self.0, ctx.to_raw_none()) }
     }
 
-    pub fn render_for_printing(&self, ctx: &mut cairo::Context) {
+    pub fn render_for_printing(&self, ctx: &cairo::Context) {
         unsafe { ffi::poppler_page_render_for_printing(self.0, ctx.to_raw_none()) }
     }
 
